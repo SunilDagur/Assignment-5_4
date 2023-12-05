1cbc3609e2990574bc3312de5d3da35cac2ee94e
Sunil Dagur <sunildagur566@gmail.com>
2023-12-05 18:55:32 +0530

    Add project files.

---
+++ Assignment 5_4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class CustomException : Exception
+{
+    public CustomException() : base("This is a custom exception.")
+    {
+       
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            
+            int someCondition = 42;
+
+            if (someCondition == 42)
+            {
+                
+                throw new CustomException();
+            }
+
+           
+
+            Console.WriteLine("No exception occurred.");
+
+        }
+        catch (CustomException ex)
+        {
+            
+            Console.WriteLine($"Custom Exception: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            
+            Console.WriteLine($"Exception: {ex.Message}");
+        }
+        finally
+        {
+            
+            Console.WriteLine("Finally block executed.");
+        }
+    }
+}
